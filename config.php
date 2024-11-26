281852369e84e73514c20a0202dea204610667d2
Masirika Constantin <elconstant73@gmail.com>
2024-11-26 18:18:24 +0100

    ddd

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-/*
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'mascodep_api');
 define('DB_USER', 'root');
-define('DB_PASS', '');*/
-ini_set('session.save_path', '/home/mascodep/sessions');
+define('DB_PASS', '');
+/*ini_set('session.save_path', '/home/mascodep/sessions');
 define('DB_HOST', '144.76.112.4');
 define('DB_NAME', 'mascodep_api');
 define('DB_USER', 'mascodep_coco');
-define('DB_PASS', 'masirika360');
+define('DB_PASS', 'masirika360');*/
 /*
 $host = '144.76.112.4';
 $nomdb = 'mascodep_api';
